c2dad85819c78c1ee4f4c4de2c02eb7364086a13
Hendrik <drikdoank@gmail.com>
2017-06-10 22:40:13 +0700

    ds

---
+++ resources/views/play.blade.php
@@ -0,0 +1,26 @@
+@include('layouts.header')
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Custom Content -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Play
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-3">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Custom Content -->
+        </div>
+    </section>
+@include('layouts.footer') 
+
