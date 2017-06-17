3d93799d4464e4e2cfa4f7a1363627bb7dcf4e67
Hendrik <drikdoank@gmail.com>
2017-06-17 22:46:47 +0700

    lovamilci

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
 <body class="login-page">
     <div class="login-box">
         <div class="logo">
-            <a href="javascript:void(0);">Admin<b>Games</b></a>
+            <a href="javascript:void(0);">Signin</b></a>
             <small>Admin Game Store - Material Design</small>
         </div>
         <div class="card">
