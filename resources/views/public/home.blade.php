1184fc8887892fa50505d5eb2b7af8e9bc887763
Hendrik <drikdoank@gmail.com>
2017-06-17 17:37:42 +0700

    lovamilci

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -109,7 +109,7 @@
                                             Top Games
                                         </div>
                                         @foreach($top_games as $top_games)
-                                        <a href="{{ route('detail.id', ['id' => $top_games->id]) }}" class="list-group-item"><h4><img class="img-circle" src="{{ asset($top_games->img) }}" width="50" height="50" >&emsp;{{ $top_games->name }}</h4></a>
+                                        <a href="{{ route('detail.id', ['id' => $top_games->id]) }}" class="list-group-item"><h4><img class="img-circle" src="{{ asset($top_games->img) }}" width="50" height="50" >&emsp;  {{ $top_games->name }}</h4><button class="btn bg-red">Play</button></a>
                                         @endforeach
                                     </div>
                                 </div>
