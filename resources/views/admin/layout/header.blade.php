9125f29ee25869ce69826c7b7614d7622811331b
Hendrik <drikdoank@gmail.com>
2017-06-25 00:15:19 +0700

    dd

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -80,7 +80,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="{{ url('/') }}">GAME STORE</a>
+                <a class="navbar-brand" href="{{ url('/') }}">Kujang Game Store (Administrator)</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
