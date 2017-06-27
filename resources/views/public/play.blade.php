6de0d795b7faf77c90d4bd5e864d825f0850a851
Hendrik <drikdoank@gmail.com>
2017-06-27 10:59:37 +0700

    dd

--- resources/views/public/play.blade.php
+++ resources/views/public/play.blade.php
@@ -1,4 +1,4 @@
-@include('layouts.header-public')
+@include('layouts.header-play')
             <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
