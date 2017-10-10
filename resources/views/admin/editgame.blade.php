ccc424fed9da29df05ab5328fbba2dfa231bcaba
Hendrik <drikdoank@gmail.com>
2017-10-10 23:10:46 +0700

    data

---
+++ resources/views/admin/editgame.blade.php
@@ -0,0 +1,170 @@
+@include('admin.layout.header')
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Custom Content -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">                        <!-- Horizontal Layout -->
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="card">
+                                    <div class="header">
+                                        <h4>
+                                            Edit Game
+                                        </h4>
+                                    </div>
+                                    <div class="body">
+                                        <form  role="form" method="POST" action="{{ url('/adddatagame') }}" enctype="multipart/form-data">
+                                            {{ csrf_field() }}
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="name">Game Name</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div class="form-line">
+                                                            <input id="name" type="text" class="form-control" name="name" value="{{ $game->name }}" placeholder="Input Your Game Name">
+                                                            @if ($errors->has('name'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                                </span>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="desc">Game Description</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div class="form-line">
+                                                            <input id="desc" type="text" class="form-control" name="desc" value="{{ $game->desc }}" placeholder="Input Your Game Description"></textarea>
+                                                            @if ($errors->has('desc'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('desc') }}</strong>
+                                                                </span>
+                                                            @endif
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="desc">URL Games</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div class="form-line">
+                                                            <input id="url" type="text" class="form-control" name="url" value="{{ $game->url }}" placeholder="Input Your Game URL"></textarea>
+                                                            @if ($errors->has('url'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('url') }}</strong>
+                                                                </span>
+                                                            @endif
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="category">Game Category</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                            <select class="form-control show-tick" name="category">
+                                                                <option value="">-- Please select --</option>
+                                                                <option @if($game->category=="Adventure") selected="selected" @endif value="Adventure">Adventure</option>
+                                                                <option @if($game->category=="Action") selected="selected" @endif value="Action">Action</option>
+                                                                <option @if($game->category=="Casual") selected="selected" @endif value="Casual">Casual</option>
+                                                                <option @if($game->category=="Puzzle") selected="selected" @endif value="Puzzle">Puzzle</option>
+                                                                <option @if($game->category=="Sports") selected="selected" @endif value="Sports">Sports</option>
+                                                                
+                                                            </select>
+                                                            </div>      
+                                                            @if ($errors->has('category'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('category') }}</strong>
+                                                                </span>
+                                                            @endif
+                                                </div>
+                                            </div>
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="price">Game Point</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div class="form-line">
+                                                            <input id="price" type="text" class="form-control" name="coint" value="{{ $game->coint }}" placeholder="Input Your Game Point">
+                                                                @if ($errors->has('price'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('price') }}</strong>
+                                                                </span>
+                                                            @endif
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="price">Game Icon (512x512)</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div >
+                                                            <input type="file" id="img" name="img" accept="image/x-png,image/gif,image/jpeg">
+                                                            @if ($errors->has('img'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('img') }}</strong>
+                                                                </span>
+                                                            @endif
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row clearfix">
+                                                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                                    <label for="price">Game Banner (1024x270)</label>
+                                                </div>
+                                                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                                    <div class="form-group">
+                                                        <div >
+                                                            <input type="file" id="banner" name="banner" accept="image/x-png,image/gif,image/jpeg">
+                                                            @if ($errors->has('banner'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('banner') }}</strong>
+                                                                </span>
+                                                            @endif
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="col-md-9 col-md-offset-3">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        <i class="fa fa-btn fa-user"></i> Add Game
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- #END# Horizontal Layout -->
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Custom Content -->
+        </div>
+    </section>
+@include('layouts.footer') 
