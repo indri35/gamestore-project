b4aee6c9d94cc0140ba01ec30f0fc3450c7f2c07
Hendrik <drikdoank@gmail.com>
2019-02-27 20:52:04 +0700

    data

--- resources/views/components/best-user.blade.php
+++ resources/views/components/best-user.blade.php
@@ -2,6 +2,6 @@
     <h3 class="gs-section__title">Best users</h3>
     <p class="gs-section__subtitle">These are the best users. Can you beat them?</p>
     <ul class="gs-list">
-        <li class="gs-list-item"><a href="{{ url('/userprofile')}}"target="_blank">Weekly</a></li>
+        <li class="gs-list-item"><a href="{{ url('/userprofile')}}"target="_blank">Monthly</a></li>
     </ul>
 </div>
